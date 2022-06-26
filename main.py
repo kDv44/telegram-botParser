69c8632a68b617dfc3df78d9efa469d6327ba5e9
ansi <mosbonaest@protonmail.com>
2022-06-26 23:09:42 +0300

    test-pars

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 import requests
 from bs4 import BeautifulSoup as b
 
-
 URL = 'https://www.autocentre.ua/avtosport'
-req = requests.get(URL)
-soup = b(req.text, 'html.parser')
 
-title_news = soup.find_all('div', class_='title')
-link_news = soup.find_all('div', class_='item')
 
-print(f"{title_news}\n{link_news}")
+class Post(object):
+    url: str
+    title: str
+    img: str
+
+    def __init__(self, url, title, img):
+        self.url = url
+        self.title = title
+        self.img = img
+
+
+def news_parser(url: str):
+    post_list: list[Post] = []
+
+    req = requests.get(url)
+    soup = b(req.text, 'html.parser')
+    find_block_news = soup.find_all('a', class_='item')
+    for elem in find_block_news:
+        post_list.append(
+            Post(
+                url=elem['href'],
+                title=elem.find('div', class_="title").text,
+                img=elem.find('div', class_="img-block")
+            )
+        )
+    return post_list
+
+
+print(f"{news_parser(URL)[0].title}\n{news_parser(URL)[0].url}\n{news_parser(URL)[0].img}")
