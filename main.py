76aac1ed4bbe67102b853a4fb6f9daf50b9fcd6c
ansi <94145647+kDv44@users.noreply.github.com>
2022-06-30 14:39:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from pars import news_parser
+from aiogram.utils.markdown import link
+
 
 URL = 'https://www.autocentre.ua/avtosport'
 
 news_list = {"news": news_parser(URL)[0].title,
              "url": news_parser(URL)[0].url,
              "img": news_parser(URL)[0].img}
+
+message_form = f"news: {news_parser(URL)[0].title}\n{link('tst', news_parser(URL)[0].url)}"
